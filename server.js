8b47fbfdd3fccc0ba60f495de143bf3a958a7d75
slayonce <Dominique.garrett1212@gmail.com>
2022-12-14 01:33:03 -0600

    fix db.json path in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 // Creates Express App
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
